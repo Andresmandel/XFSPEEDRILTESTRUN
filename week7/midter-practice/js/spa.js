622faaae4c3f1c2dec869f724a27504404104881
Andresmandel <54686465+Andresmandel@users.noreply.github.com>
2019-10-19 16:58:53 -0400

    i did something

---
+++ week7/midter-practice/js/spa.js
@@ -0,0 +1,56 @@
+var app = angular.module('customersApp',['ngRoute']);
+            app.config(function($routeProvider) {
+              $routeProvider.when('/',
+                {
+                  controller:'CustomersController',
+                  templateUrl: 'app/views/customers.html'
+                })
+              .when('/experience',
+                {
+                  controller: 'ExperienceController',
+                  templateUrl: 'app/views/experience.html'
+                })
+                .when('/request',
+                  {
+                    controller: 'RequestController',
+                    templateUrl: 'app/views/request.html'
+                  })
+               .when('/dealer',
+                  {
+                    controller: 'DealerController',
+                    templateUrl: 'app/views/dealer.html'
+                  })
+               .when('/story',
+                  {
+                    controller: 'StoryController',
+                    templateUrl: 'app/views/story.html'
+                  })
+              .when('/adam',
+                 {
+                   controller: 'AdamController',
+                   templateUrl: 'app/views/adam.html'
+                 })
+            });
+            app.controller('ExperienceController', function($scope){
+              $scope.customerID = 5;
+            });
+            app.controller('RequestController', function($scope){
+              $scope.customerID = 5;
+            });
+            app.controller('StoryController', function($scope){
+              $scope.customerID = 5;
+            });
+            app.controller('DealerController', function($scope){
+              $scope.customerID = 5;
+            });
+            app.controller('AdamController', function($scope){
+              $scope.customerID = 5;
+            });
+            app.controller('CustomersController', function($scope) {
+              $scope.customers = [
+                  {"id": 1, "name":"Ted", "total": 5.996},
+                  {"id": 2, "name":"Michelle", "total": 10.994},
+                  {"id": 3, "name":"Zed", "total": 10.99},
+                  {"id": 4, "name":"Tina", "total": 15.994}
+                ];
+              });
